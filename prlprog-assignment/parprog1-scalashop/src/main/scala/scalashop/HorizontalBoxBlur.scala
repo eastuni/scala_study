b97e327d69d6afe773978a0906df5bbc4f5fbe4e
donghyun <donghyun@donghyun-PC>
2016-11-17 14:21:51 +0900

    new

---
+++ prlprog-assignment/parprog1-scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -0,0 +1,71 @@
+package scalashop
+
+import org.scalameter._
+import common._
+
+object HorizontalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      HorizontalBoxBlur.blur(src, dst, 0, height, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      HorizontalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+}
+
+
+/** A simple, trivially parallelizable computation. */
+object HorizontalBoxBlur {
+
+  /**
+   * Blurs the rows of the source image `src` into the destination image `dst`,
+   *  starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each row, `blur` traverses the pixels by going from left to right.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for {
+      j <- from until end
+      i <- 0 until src.width
+    } {
+      dst.update(i, j, boxBlurKernel(src, i, j, radius))
+    }
+  }
+
+  /**
+   * Blurs the rows of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  rows.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    var step = src.height/numTasks
+    if(step < 1) step = 1
+    val r1 = (0 to src.height by step).toList
+    val tasks = r1 zip r1.tail
+    tasks.map {
+      case (from, end) => task { blur(src, dst, from, end, radius) }.join
+    }
+  }
+
+}
